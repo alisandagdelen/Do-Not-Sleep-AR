0c6b6306ba77f21ba54dd280ac0d0ee277311169
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-01-08 20:47:45 +0300

    Initial Commit

---
+++ Do Not Sleep AR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do Not Sleep AR
+//
+//  Created by alisandagdelen on 8.01.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Do Not Sleep ARTests/Do_Not_Sleep_ARTests.swift
@@ -0,0 +1,36 @@
+//
+//  Do_Not_Sleep_ARTests.swift
+//  Do Not Sleep ARTests
+//
+//  Created by alisandagdelen on 8.01.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import XCTest
+@testable import Do_Not_Sleep_AR
+
+class Do_Not_Sleep_ARTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
